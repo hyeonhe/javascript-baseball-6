e268d63d459b4809a70cff3b24b115edac7d3681
hyeonhe <63030323+hyeonhe@users.noreply.github.com>
2023-12-01 13:42:28 +0900

    refactor: import 수정

--- src/utils/generateComputerNumber.js
+++ src/utils/generateComputerNumber.js
@@ -1,13 +1,11 @@
-import { MissionUtils } from "@woowacourse/mission-utils";
+import { Random } from "@woowacourse/mission-utils";
 import { NUMBER_LENGTH, MIN_NUMBER, MAX_NUMBER } from "../constants/constants";
+
 export default function generateComputerNumber() {
   const computer = [];
 
   while (computer.length < NUMBER_LENGTH) {
-    const number = MissionUtils.Random.pickNumberInRange(
-      MIN_NUMBER,
-      MAX_NUMBER
-    );
+    const number = Random.pickNumberInRange(MIN_NUMBER, MAX_NUMBER);
     if (!computer.includes(number)) {
       computer.push(number);
     }
